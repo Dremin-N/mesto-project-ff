ae06d55dbb79a6c0a5bc7d2b78690218fdb84cdd
Dremin-N <dr.kolka19@gmail.com>
2024-07-18 23:26:24 +0500

    add functions for add and delete cards

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+[Cсылка на репозиторий](https://github.com/Dremin-N/mesto-project-ff.git)
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+function createCard(data, deleteButton) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция создания карточки
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.innerText = data.name;
 
-// @todo: Функция удаления карточки
+  cardDeleteButton.addEventListener("click", () => {
+    deleteButton(cardElement);
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+function onDelete(elem) {
+  elem.remove();
+}
+
+function renderedCards(dataCards) {
+  dataCards.forEach((item) => {
+    placesList.append(createCard(item, onDelete));
+  });
+}
+
+renderedCards(initialCards);
